b97b46af18d35ef803a4b23a41a3bc77c18b76eb
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-01-17 14:37:16 +0600

    Tortinshi commit

---
+++ Programm_3.cpp
@@ -0,0 +1,16 @@
+//Бариев Абылайхан
+//No3 Экраннан 1 жол оқып алыңыз. Сол жолда қанша әріп бар екенін анықтаңыз.
+
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main() 
+{
+    string a;
+    cin >> a;
+    int b = a.size();
+    cout << b;
+    return 0;
+}
