70018baa1f53096ebc09d2cb0828747bd79d7bf2
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-01-17 14:49:19 +0600

    Besinshi commit

---
+++ programm_4.cpp
@@ -0,0 +1,20 @@
+//Бариев Абылайхан
+//No4 Матрицаның қай жол элементтерінің қосындысы ең үлкен екенін анықтау керек. 
+//Егер бірнеше жолдың қосындысы тең болса, жол нөмірі үлкенін шығару қажет.
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main()
+{
+   string s;
+ 
+    cout<<"Input string: "; getline(cin, s);
+ 
+     cout<<"Chars - "<<s.length()<<endl;
+ 
+   system("pause");
+ 
+  return 0;
+}
